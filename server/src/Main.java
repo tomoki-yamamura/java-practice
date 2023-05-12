d440e6b817e366cdb0e00eadb334033edd4ac39a
tomoki-yamamura <progtab17@gmail.com>
2023-05-13 02:09:51 +0900

    fin2

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-      int x = 5;
-      int y = 10;
-      String ans = "x + yは" + (x + y);
-      System.out.println(ans);
+      int[] scores = {1,2,3,4,5};
+      for (int value: scores) {
+        System.out.println(value);
+      }
     }
 }
